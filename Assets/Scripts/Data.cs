89ff83cfcb193867ed0f2deaa9e929d33b8c42e9
shiyuanGame <shiyuanGames@gmail.com>
2023-09-23 15:08:57 +0800

    update

---
+++ Assets/Scripts/Data.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+/// <summary>
+/// 界面编号（对应界面配置表上的界面编号。
+/// </summary>
+public enum UIFormId
+{
+    Undefined = 0,
+
+
+    /// <summary>
+    /// 菜单界面
+    /// </summary>
+    MenuForm = 1,
+
+    /// <summary>
+    /// 设置界面
+    /// </summary>
+    SettingForm = 2,
+
+    /// <summary>
+    /// 积分界面
+    /// </summary>
+    ScoreForm = 3,
+
+    /// <summary>
+    /// 游戏结束界面
+    /// </summary>
+    GameOverForm = 4
+}
+public class Data : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
